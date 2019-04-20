9a295040bd6456a625f6920b4911a4aeae9f5ff8
lkh_home <a47165705@gmail.com>
2019-04-20 18:32:03 +0900

    ajax 작업 중

--- src/main/java/com/testboard/controller/user/LoginController.java
+++ src/main/java/com/testboard/controller/user/LoginController.java
@@ -1,11 +1,15 @@
 package com.testboard.controller.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.testboard.biz.user.UserService;
 import com.testboard.biz.user.UserVO;
@@ -22,11 +26,20 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/login.do", method=RequestMethod.POST)
-	public String login(UserVO vo, HttpSession session) {
+	@ResponseBody
+	public Map<String, Object> login(UserVO vo, HttpSession session) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		
 		if(vo.getUserId() == null || vo.getUserId().equals("")) {
-			throw new IllegalArgumentException("아이디를 입력해주세요.");
+			result.put("result", false);
+			result.put("message", "아이디를 입력해주세요.");
+			
+			return result;
 		} else if(vo.getPassword() == null || vo.getPassword().equals("")) {
-			throw new IllegalArgumentException("비밀번호를 입력해주세요.");
+			result.put("result", false);
+			result.put("message", "비밀번호를 입력해주세요.");
+			
+			return result;
 		}
 		
 		UserVO user = userService.getUserInfo(vo);
@@ -34,9 +47,20 @@ public class LoginController {
 			session.setAttribute("name", vo.getName());
 			session.setAttribute("userId", vo.getUserId());
 			
-			return "redirect:getBoardList.do";
+			if(vo.getPassword().equals(user.getPassword())) {
+				result.put("result", true);
+				result.put("message", "");
+			} else {
+				result.put("result", false);
+				result.put("message", "비밀번호가 일지하지 않습니다.");
+			}
+			
+			return result;
 		} else {
-			return "login";
+			result.put("result", false);
+			result.put("message", "등록된 아이디가 없습니다.");
+			
+			return result;
 		}
 	}
 	
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,9 +1,11 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
-<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type="text/javascript" src="<c:url value="/resources/js/lib/jquery-3.3.1.js"/>"></script>
+<script type="text/javascript" src="<c:url value="/resources/js/login.js"/>"></script>
 <title>로그인</title>
 </head>
 <body>
@@ -11,7 +13,8 @@
 <center>
 	<h1>로그인</h1>
 	<hr>
-	<form action="login.do" method="post">
+	<!-- <form action="login.do" method="post"> -->
+	<form>
 		<table border="1" cellpadding="0" cellspacing="0">
 			<tr>
 				<td bgcolor="orange">아이디 : </td>
@@ -23,7 +26,7 @@
 			</tr>
 			<tr>
 				<td colspan="2" align="center">
-					<input type="submit" value="로그인"/>
+					<input type="button" id="btnLogin" value="로그인"/>
 				</td>
 			</tr>
 		</table>
